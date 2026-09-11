b5718e07c6891152b9358250c273aca23efeedce
odounlami <67371145+odounlami@users.noreply.github.com>
2026-09-11 03:27:45 +0100

    feat: configure Nest application

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: [
+      'https://iamthegood.vercel.app',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+    ],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(config.get<number>('PORT') ?? 3000);
+}
+
+bootstrap();
