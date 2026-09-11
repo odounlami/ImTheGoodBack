281ab01042e47ce5d60e689266aa3158dbcf32d6
odounlami <67371145+odounlami@users.noreply.github.com>
2026-09-11 22:16:38 +0100

    feat: protect auth endpoints from brute force

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
+@UseGuards(ThrottlerGuard)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
