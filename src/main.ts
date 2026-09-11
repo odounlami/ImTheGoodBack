c83a3f5ad8e3bd71ed514d8d56d6ac36f1b55c84
odounlami <67371145+odounlami@users.noreply.github.com>
2026-09-11 22:16:58 +0100

    docs: expose swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -24,6 +25,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ImTheGood API')
+    .setDescription('API documentation for ImTheGood')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(config.get<number>('PORT') ?? 3000);
 }
